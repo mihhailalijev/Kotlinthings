240b8c24c9fb62019698dba0f5f94fc5bc6fcb3c
Misha <mihhail.alijev@bolt.eu>
2019-12-08 19:35:16 +0200

    Redesigned grid layout, added alert dialog to log out action

--- app/src/main/java/com/example/kotlinthings/GalleryActivity.kt
+++ app/src/main/java/com/example/kotlinthings/GalleryActivity.kt
@@ -1,7 +1,6 @@
 package com.example.kotlinthings
 
 import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -11,7 +10,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.PopupMenu
 import android.widget.Toast
-
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
